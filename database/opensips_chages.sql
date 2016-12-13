98360af55c841f14d360079c33382ed9b41499e4
harsh <harsh.patel@inextrix.com>
2016-12-13 15:23:39 +0530

    [FIX] Some fields missing in opensip table #7558

--- database/opensips_chages.sql
+++ database/opensips_chages.sql
@@ -5,3 +5,8 @@ ALTER TABLE `subscriber` ADD `status` TINYINT( 1 ) NOT NULL DEFAULT '0' COMMENT 
 ADD `creation_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
 ADD `last_modified_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
 ALTER TABLE `subscriber` ADD `reseller_id` INT( 4 ) NOT NULL ;
+
+
+ALTER TABLE `subscriber` ADD `channel_limit` INT( 5 ) NOT NULL ,
+ADD `effective_caller_id_name` VARCHAR( 50 ) NOT NULL ,
+ADD `effective_caller_id_number` VARCHAR( 50 ) NOT NULL ;
